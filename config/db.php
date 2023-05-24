fc06f03a6c0d62c79acb250a60c0923751646eb8
Z1nRes <tv.noob@bk.ru>
2023-05-24 05:24:30 +0300

    rewrite User model

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=csgoteam',
     'username' => 'root',
-    'password' => '',
+    'password' => 'QWEasd123',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
